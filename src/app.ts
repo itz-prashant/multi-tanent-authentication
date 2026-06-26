8be9c241385cf55be5bf473b8a2f84d62febda8f
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-06-26 18:51:33 +0530

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    return res.send("Welcome to auth service");
+    res.send("Welcome to auth service");
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    void next;
+    console.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
